53bc27f2c68d4360d8e8261822b5f8e15f90d902
Jonathan Katz <jkatz5671@gmail.com>
2017-01-24 13:24:45 -0500

    plotted data!

---
+++ Rprog/hospital readmit outcome/Hospital readmit correlation.R
@@ -0,0 +1,4 @@
+setwd("~/IntroDS/IntroDS_Pierrepont/Rprog/hospital readmit outcome/")
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+readmit <- read.csv("Hospital_Readmissions_Reduction_Program.csv", colClasses = "character")
+
